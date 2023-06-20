68b144531de9d7d3ec9c2f37886eabb896f2097f
KonQ1999 <michal.drago@gmail.com>
2023-06-20 02:36:33 +0200

    Seeder update

--- src/main/java/pb/edu/airportproject/Seeder.java
+++ src/main/java/pb/edu/airportproject/Seeder.java
@@ -20,10 +20,12 @@ public class Seeder implements CommandLineRunner {
     @Override
     public void run(String... args) {
         deleteAllFromRepository(flightRepository);
-        Flight flight1 = new Flight(1l, "Białystok", "Warszawa",  LocalDateTime.now(), 4);
-        Flight flight2 = new Flight(2l, "Białystok", "Kielce",  LocalDateTime.now(), 100);
-        Flight flight3 = new Flight(3l, "Białystok", "Lublin",  LocalDateTime.now(), 100);
-        Flight flight4 = new Flight(4l, "Madryt", "Bialystok",  LocalDateTime.now(), 100);
+
+
+        Flight flight1 = new Flight(1l, "Białystok", "Warszawa",  LocalDateTime.of(2023, 7, 28, 10, 33, 38, 640000) , 40);
+        Flight flight2 = new Flight(2l, "Białystok", "Kielce",  LocalDateTime.of(2024, 8, 14, 14, 19, 28, 640000), 80);
+        Flight flight3 = new Flight(3l, "Białystok", "Lublin",  LocalDateTime.of(2023, 9, 15, 12, 21, 48, 640000), 50);
+        Flight flight4 = new Flight(4l, "Madryt", "Bialystok",  LocalDateTime.of(2024, 12, 28, 9, 5, 18, 640000), 100);
         Flight flight5 = new Flight(5l, "Białystok", "Warszawa",  LocalDateTime.now(), 100);
 
         flightRepository.save(flight1);
